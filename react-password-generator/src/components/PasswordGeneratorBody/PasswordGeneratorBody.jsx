98b5719c29ae00bf5a357e1e70cb145e94b77cba
gselvara@yahoo.com <gselvara@yahoo.com>
2024-01-30 22:11:18 -0800

    more changes for password project

--- react-password-generator/src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
+++ react-password-generator/src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
@@ -1,7 +1,34 @@
 import theStle from "./style.module.css";
 
 export function PasswordGeneratorBody() {
-    return <div>
-        PasswordGeneratorBody
-    </div>
+    return (
+        <form>
+            <div className={theStle.grid_container}>
+                <div className={theStle.grid_item}>
+                    Size
+                    <select defaultValue={15}>
+                        <option>5</option>
+                        <option>10</option>
+                        <option>15</option>
+                        <option>20</option>
+                    </select>
+                </div>
+                <div className={theStle.grid_item}>
+                    Uppercase
+                    <input type="checkbox" defaultChecked/>
+                </div>
+                <div className={theStle.grid_item}>
+                    Numbers
+                    <input type="checkbox" defaultChecked/>
+                </div>
+                <div className={theStle.grid_item}>
+                    Specials
+                    <input type="checkbox" defaultChecked/>
+                </div>
+            </div>
+            <div className={theStle.btn_container}>
+                <button className={theStle.btn_generate}>GENERATE</button>
+            </div>
+        </form>
+    )
 }
--- react-password-generator/src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
+++ react-password-generator/src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
@@ -1,4 +1,8 @@
 import theStle from "./style.module.css";
+import lockImage from "../../assets/lock.svg"
 export function PasswordGeneratorHeader() {
-    return <div>PasswordGeneratorHeader</div>
+    return <div className={theStle.root }>
+        <div className={theStle.title}>Password Generator</div>
+        <img src={lockImage} className={theStle.icon } alt="Lock Icon"/>
+    </div>
 }
